819710f3e2bd59862eeed3b63f300004ed665091
Vitor Gomes <vitor.gomes@vgct.com.br>
2019-03-15 08:04:40 +0000

    Add project files.

---
+++ FibonacciNumbers/CalculateFibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    public class CalculateFibonacci
+    {
+        //{ F_{0}=0}
+        internal Int32 ReturnedValue = 0;
+        public CalculateFibonacci()
+        {
+        }
+
+        public int Calculate(int steps)
+        {
+            if (steps == 1 || steps == 2)
+            {
+                //{ F_{1}=1,}
+                ReturnedValue = 1;
+            }else if (steps > 2)
+            {
+                //{F_{n}=F_{n-1}+F_{n-2},} https://en.wikipedia.org/wiki/Fibonacci_number
+                ReturnedValue = Calculate(steps -1) + Calculate(steps - 2);
+            }
+
+            return ReturnedValue;
+        }
+    }
+}
